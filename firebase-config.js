e1462bf13b63335a6d0e287e0aa0457d50967ab7
Aman Sharma <145845470+aman-sharma-2310@users.noreply.github.com>
2026-02-08 18:22:53 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
 import { getFirestore }  from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
 const firebaseConfig = {
-  apiKey: "AIzaSyDyDC1SLQDf_7YswrbTadP4Q-LbgjsEStw",
-  authDomain: "sparkbill-eb3a4.firebaseapp.com",
-  projectId: "sparkbill-eb3a4",
-  storageBucket: "sparkbill-eb3a4.firebasestorage.app",
-  messagingSenderId: "1031654652067",
-  appId: "1:1031654652067:web:678a0c7f0b60593eb52716",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // 🔥 Initialize Firebase
